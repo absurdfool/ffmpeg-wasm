cc7fdf5949051f1d9aece9d0b716603b50180c25
AkihiroOgawa <ogawa,akihiro@fujitsu.com>
2022-10-12 04:21:45 +0900

    test

--- pages/_worker.js
+++ pages/_worker.js
@@ -1,4 +1,4 @@
-var ffmpeg_wasm_default = {
+default export {
 	async fetch(request,env) {
 		const res = env.ASSETS.fetch(request);
 		res.headers.append("Cross-Origin-Embedder-Policy", "require-corp");
