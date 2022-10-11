bdc57551f203069790b8d646b081f5ee365c4176
AkihiroOgawa <ogawa,akihiro@fujitsu.com>
2022-10-12 03:59:22 +0900

    test

--- pages/_worker.js
+++ pages/_worker.js
@@ -1,13 +1,15 @@
-export {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith('/api/')) {
-      // TODO: Add your custom /api/* logic here.
-      return new Response('Ok');
-    }
-    // Otherwise, serve the static assets.
-    // Without this, the Worker will error and no assets will be served.
-    return env.ASSETS.fetch(request);
-  },
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
-};
+async function handleRequest(request) {
+
+  let originalResponse = await fetch(request)
+
+  let response = new Response(originalResponse.body,originalResponse);
+
+  response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+  response.headers.set('Cross-Origin-Opener-Policy','same-origin');
+
+  return response
+}
