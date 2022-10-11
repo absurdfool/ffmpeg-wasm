21b3f3998ec37059b4c14d4eb6c6f587bf2caa5b
AkihiroOgawa <ogawa,akihiro@fujitsu.com>
2022-10-12 01:11:44 +0900

    worker.ts move

--- src/worker.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	async fetch(request,env) {
-		const res = env.ASSETS.fetch(request);
-		res.headers.append("Cross-Origin-Embedder-Policy", "require-corp");
-		res.headers.append("Cross-Origin-Opener-Policy", "same-origin");
-		return res;
-	},
-};
