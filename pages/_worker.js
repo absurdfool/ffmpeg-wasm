36606153d9c9dc6564195cce5c20b62d6b9de060
AkihiroOgawa <ogawa,akihiro@fujitsu.com>
2022-10-12 12:26:10 +0900

    _worker.js delete

--- pages/_worker.js
+++
@@ -1,8 +0,0 @@
-default export {
-	async fetch(request,env) {
-		const res = env.ASSETS.fetch(request);
-		res.headers.append("Cross-Origin-Embedder-Policy", "require-corp");
-		res.headers.append("Cross-Origin-Opener-Policy", "same-origin");
-		return res;
-	},
-};
